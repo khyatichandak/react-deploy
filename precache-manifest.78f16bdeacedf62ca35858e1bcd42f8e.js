4fc007ae8aafb059fcbc16ae702da6599117cffb
khyatichandak <khyati.agola@gmail.com>
2020-06-26 17:28:08 -0400

    Updates

---
+++ precache-manifest.78f16bdeacedf62ca35858e1bcd42f8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9545bccb4ae19f0f105f8f9a76097901",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61b42a45abda6f269744",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "340aac5933c756125f93",
+    "url": "/static/js/2.a0c31166.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a0c31166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61b42a45abda6f269744",
+    "url": "/static/js/main.9ec898c9.chunk.js"
+  },
+  {
+    "revision": "53ff9637c311bd424e1b",
+    "url": "/static/js/runtime-main.5a7750ca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
